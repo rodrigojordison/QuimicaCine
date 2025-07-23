f0002b85d70c1cf54b9f6d3ec6a5b42022f09428
rodrigojordison <rodrigojordisonsilva@gmail.com>
2025-07-23 17:20:27 -0300

    commitcine

--- main.js
+++ main.js
@@ -28,3 +28,34 @@ window.addEventListener("scroll", () => {
 
     lastScrollTop = scrollTop;
 });
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+    menu.classList.toggle('bx-x'); // Ícone muda para "X"
+};
+
+// Fechar menu ao clicar em um link
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    menu.classList.remove('bx-x');
+};
+
+// Swiper - Carrossel principal
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    grabCursor: true,
+});
+
